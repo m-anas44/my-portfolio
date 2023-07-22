51e4138d85e4a1bddebf198421e4cc9acb771a4a
m-anas44 <125805038+m-anas44@users.noreply.github.com>
2023-07-22 22:40:36 +0500

    Edited

--- src/app/components/type.tsx
+++ src/app/components/type.tsx
@@ -2,17 +2,17 @@
 import React from 'react';
 import Typewriter from 'typewriter-effect';
 const Type = () => {
-  return (
-    <div>
-        <Typewriter
-                            options={{
-                                strings: ['Web Designer', 'UI/UX Designer'],
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        />
-    </div>
-  )
+    return (
+        <div>
+            <Typewriter
+                options={{
+                    strings: ['Web Designer', 'UI/UX Designer'],
+                    autoStart: true,
+                    loop: true,
+                }}
+            />
+        </div>
+    )
 }
 
 export default Type
