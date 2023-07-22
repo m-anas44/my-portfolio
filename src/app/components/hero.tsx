de7e25d998cd34dddd91919d4e6cc5f7bc3ddab9
m-anas44 <125805038+m-anas44@users.noreply.github.com>
2023-07-22 22:28:34 +0500

    new

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -5,7 +5,7 @@ import Type from './type';
 
 const Hero = () => {
     return (
-        <section className="text-gray-600 body-font bg-fixed bg-cover bg-center custom-image">
+        <section className="text-gray-600 body-font bg-fixed bg-cover bg-center">
             <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
                     <Image
@@ -16,7 +16,7 @@ const Hero = () => {
                 </div>
                 <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                        Hi! I'm
+                        Hi I am
                         <br className="hidden lg:inline-block" />
                         <Type />
                     </h1>
