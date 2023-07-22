2c071ab100128cec9f0cd2bc1efb278dced8360b
m-anas44 <125805038+m-anas44@users.noreply.github.com>
2023-07-22 22:25:22 +0500

    New Edit

---
+++ src/app/components/type.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+const Type = () => {
+  return (
+    <div>
+        <Typewriter
+                            options={{
+                                strings: ['Web Designer', 'UI/UX Designer'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+    </div>
+  )
+}
+
+export default Type
